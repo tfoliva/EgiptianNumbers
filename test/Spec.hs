7e11a0b9511873ac33bb3e3aae78365832ed9cdd
Tiago Oliva <tiago.oliva.costa@gmail.com>
2021-11-24 22:01:57 -0300

    feat(test): write test cases sketch

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
 main :: IO ()
+
+-- Test cases:
+    -- Input | Output
+    -- 0/1 == 0/1
+    -- 1/2 == 1/2
+    -- 2/3 == 1/3 + 1/3
+    -- 5/6 == 1/2 + 1/3
+    -- 7/13 == ?
+    -- Test using float for large fractions
+    -- Test with random fractions
 main = putStrLn "Test suite not yet implemented"
